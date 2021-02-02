8f65e28dff47c9007bf7f8d0dd6f3cff979dec45
SaiyanShivvy <hdiproductionsoffical@gmail.com>
2021-02-03 02:40:01 +1300

    Updated PokemonDetail.vue. Accessed and Assigned Basic Info, Evolution chain, species, etc. to be added next.

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -17,11 +17,6 @@
           <hr />
           <div class="ml-auto">
             <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Agree
-            </button>
-            <button
               class="bg-transparent hover:bg-gray-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
               @click.prevent="close"
             >
@@ -51,38 +46,114 @@ export default {
       this.$emit("close");
     }
   },
-  //stats
-  //types
-  //weight
-  //height
-  //Species
-  //catch rate
-  //egg groups, hatch rate
-  // ev yeild
-  //national dex number
-  //evolution data
-  //flavor text
   setup(props) {
     const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/" + props.pokemon;
-    var pkm = reactive({
-      pokeData: []
+
+    const speciesUrl =
+      "https://pokeapi.co/api/v2/pokemon-species/" + props.pokemon;
+
+    const encounterLocationsUrl =
+      "https://pokeapi.co/api/v2/pokemon/" + props.pokemon + "/encounters";
+
+    const evolutionChainUrl =
+      "https://pokeapi.co/api/v2/evolution-chain/" + props.pokemon;
+
+    console.log(speciesUrl, encounterLocationsUrl, evolutionChainUrl);
+
+    // Main Details
+    var pokeInfo = reactive({
+      nationalDex: Number,
+      ability: [],
+      hiddenAbility: [],
+      types: [],
+      height: Number,
+      weight: Number,
+      hp: Number,
+      attack: Number,
+      defense: Number,
+      specialAttack: Number,
+      specialDefense: Number,
+      speed: Number,
+      effortValue: Number,
+      effortStat: String,
+      baseExp: Number,
+      moves: []
     });
+
+    //Species, Flavor Text and Evolution Details
+
+    //Encounter Locations
+
     //Check Mount
     onMounted(() => {
       getPokemonData();
-      console.log(props.pokemon);
     });
 
     function getPokemonData() {
+      // Currently everything is acccessed and assigned here
+      // may create additional function to access and assign seperate values
       fetch(pokemonUrl)
         .then(res => res.json())
         .then(data => {
-          pkm.pokeData = data;
-          console.log(pkm);
+          //Get Abilities
+          if (data.abilities.length > 0) {
+            for (let i = 0; i < data.abilities.length; i++) {
+              if (data.abilities[i].is_hidden) {
+                pokeInfo.hiddenAbility.push(data.abilities[i].ability.name);
+              } else {
+                pokeInfo.ability.push(data.abilities[i].ability.name);
+              }
+            }
+          }
+          //Get Moves
+          if (data.moves.length > 0) {
+            for (let i = 0; i < data.moves.length; i++) {
+              pokeInfo.moves.push(data.moves[i].move.name);
+            }
+          }
+          //Get Types
+          if (data.types.length > 0) {
+            for (let i = 0; i < data.types.length; i++) {
+              pokeInfo.types.push(data.types[i].type.name);
+            }
+          }
+          //Get Stats and EVs
+          if (data.stats.length > 0) {
+            for (let i = 0; i < data.stats.length; i++) {
+              if (data.stats[i].effort != 0) {
+                pokeInfo.effortValue = data.stats[i].effort;
+                pokeInfo.effortStat = data.stats[i].stat.name;
+              }
+              if (data.stats[i].stat.name == "hp") {
+                pokeInfo.hp = data.stats[i].base_stat;
+              } else if (data.stats[i].stat.name == "attack") {
+                pokeInfo.attack = data.stats[i].base_stat;
+              } else if (data.stats[i].stat.name == "defense") {
+                pokeInfo.defense = data.stats[i].base_stat;
+              } else if (data.stats[i].stat.name == "special-attack") {
+                pokeInfo.specialAttack = data.stats[i].base_stat;
+              } else if (data.stats[i].stat.name == "special-defense") {
+                pokeInfo.specialDefense = data.stats[i].base_stat;
+              } else if (data.stats[i].stat.name == "speed") {
+                pokeInfo.speed = data.stats[i].base_stat;
+              }
+            }
+          }
+          // Get Height and weight
+          pokeInfo.height = data.height;
+          pokeInfo.weight = data.weight;
+
+          //Get Natinoal Dex
+          pokeInfo.nationalDex = data.id.toFixed();
+
+          //Get Base Exp
+          pokeInfo.baseExp = data.base_experience;
+
+          console.log(pokeInfo);
         });
     }
 
-    return { getPokemonData };
+    return { getPokemonData, pokeInfo };
   }
 };
 </script>
